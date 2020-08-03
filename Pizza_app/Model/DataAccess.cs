43dd8a9698459e9e3f21bf26761d9b88aba91df2
jasminakostadinovic <jasmina.kostadinovic26@gmail.com>
2020-08-03 11:42:47 +0200

    db model added

---
+++ Pizza_app/Model/DataAccess.cs
@@ -0,0 +1,178 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Pizza_app.Model
+{
+	class DataAccess
+    {
+		internal List<string> LoadPersonalNumbers()
+		{
+			using (var context = new PizzaRestaurantEntities())
+			{
+				var personalNumbers = new List<string>();
+				if (context.tblOrders.Any())
+				{
+					foreach (var item in context.tblOrders)
+					{
+						personalNumbers.Add(item.OrdererPersonalNo);
+					}
+					return personalNumbers;
+				}
+				return personalNumbers;
+			}
+		}
+
+		internal List<tblMeal> LoadMeals()
+		{
+			using (var context = new PizzaRestaurantEntities())
+			{
+				var meals = new List<tblMeal>();
+
+				if (context.tblMeals.Any())
+				{
+					foreach (var item in context.tblMeals)
+					{
+						meals.Add(item);
+					}
+					return meals;
+				}
+				return meals;
+			}
+		}
+
+		internal bool IsExistingMeal(int mealID)
+		{
+			using (var context = new PizzaRestaurantEntities())
+			{
+				return context.tblMeals.Any(e => e.MealID == mealID);
+			}
+		}
+
+		internal List<tblOrder> LoadAllOrders()
+		{
+			using (var context = new PizzaRestaurantEntities())
+			{
+				var orders = new List<tblOrder>();
+
+				if (context.tblOrders.Any())
+				{
+					foreach (var item in context.tblOrders)
+					{
+						orders.Add(item);
+					}
+					return orders;
+				}
+				return orders;
+			}
+		}
+
+		internal List<tblOrder> LoadOrders(string ordererPersonalNo)
+		{
+			using (var context = new PizzaRestaurantEntities())
+			{
+				var orders = new List<tblOrder>();
+
+				if (context.tblOrders.Any(x => x.OrdererPersonalNo == ordererPersonalNo))
+				{
+					foreach (var item in context.tblOrders.Where(x => x.OrdererPersonalNo == ordererPersonalNo))
+					{
+						orders.Add(item);
+					}
+					return orders;
+				}
+				return orders;
+			}
+		}
+
+		internal void AddNewOrder(tblOrder newOrder)
+		{
+			using (var conn = new PizzaRestaurantEntities())
+			{
+				conn.tblOrders.Add(newOrder);
+				conn.SaveChanges();
+			}
+		}
+
+		internal List<tblOrder> LoadOrders()
+		{
+			using (var conn = new PizzaRestaurantEntities())
+			{
+				var orders = new List<tblOrder>();
+				if (conn.tblOrders.Any())
+					return conn.tblOrders.ToList();
+				return orders;
+			}
+		}
+
+		internal void AddNewOrderedMeal(tblMealOrder mealOrder)
+		{
+			using (var conn = new PizzaRestaurantEntities())
+			{
+				conn.tblMealOrders.Add(mealOrder);
+				conn.SaveChanges();
+			}
+		}
+
+		internal bool IsExistingOrder(int orderID)
+		{
+			using (var context = new PizzaRestaurantEntities())
+			{
+				return context.tblOrders.Any(e => e.OrderID == orderID);
+			}
+		}
+
+		internal void DeleteOrder(int orderID)
+		{
+			using (var context = new PizzaRestaurantEntities())
+			{
+				var orderToRemove = context.tblOrders.FirstOrDefault(e => e.OrderID == orderID);
+				if (orderToRemove != null)
+				{
+					context.tblOrders.Remove(orderToRemove);
+					context.SaveChanges();
+				}
+			}
+		}
+
+		internal List<tblMealOrder> LoadOrderedMeals(int orderID)
+		{
+			using (var conn = new PizzaRestaurantEntities())
+			{
+				var orderedMeals = new List<tblMealOrder>();
+				if (conn.tblOrders.Any(x => x.OrderID == orderID))
+				{
+					foreach (var item in conn.tblMealOrders.Where(x => x.OrderID == orderID))
+					{
+						orderedMeals.Add(item);
+					}
+					return orderedMeals;
+				}
+
+				return orderedMeals;
+			}
+		}
+
+		internal void DeleteOrderedMeal(tblMealOrder mealOrder)
+		{
+			using (var context = new PizzaRestaurantEntities())
+			{
+				var orderToRemove = context.tblMealOrders.FirstOrDefault(e => e.MealOrderID == mealOrder.MealOrderID);
+				if (orderToRemove != null)
+				{
+					context.tblMealOrders.Remove(orderToRemove);
+					context.SaveChanges();
+				}
+			}
+		}
+
+		internal void UpdateOrder(tblOrder orderToUpdate)
+		{
+			using (var context = new PizzaRestaurantEntities())
+			{
+				context.tblOrders.First(e => e.OrderID == orderToUpdate.OrderID).IsApproved = orderToUpdate.IsApproved;
+
+				context.SaveChanges();
+			}
+		}
+	}
+}
---
+++ Pizza_app/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,110 @@
+﻿using Pizza_app.Command;
+using Pizza_app.Model;
+using Pizza_app.View;
+using Pizza_app.View.Employees;
+using Pizza_app.View.Guests;
+using Pizza_app.View.Orders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Threading;
+using Validations;
+
+namespace Pizza_app.ViewModel
+{
+    class MainWindowViewModel : ViewModelBase
+	{
+		#region Fields
+		private string userName;
+		readonly MainWindow loginView;
+		#endregion
+
+		#region Constructor
+		internal MainWindowViewModel(MainWindow view)
+		{
+			this.loginView = view;
+		}
+		#endregion
+
+		#region Properties
+		public string UserName
+		{
+
+			get
+			{
+				return userName;
+			}
+			set
+			{
+				userName = value;
+				OnPropertyChanged(nameof(UserName));
+			}
+		}
+		#endregion
+
+		private ICommand submitCommand;
+		public ICommand SubmitCommand
+		{
+			get
+			{
+				if (submitCommand == null)
+				{
+					submitCommand = new RelayCommand(Submit);
+					return submitCommand;
+				}
+				return submitCommand;
+			}
+		}
+
+		void Submit(object obj)
+		{
+			string password = (obj as PasswordBox).Password;
+			var validate = new Validations.Validations();
+			if (UserName == "Zaposleni" && password == "Zaposleni")
+			{
+				EmployeeView employeeView = new EmployeeView(UserName);
+				loginView.Close();
+				employeeView.Show();
+				return;
+			}
+
+			else if (validate.IsValidPersonalNoFormat(UserName) && password == "Gost")
+			{
+
+				DataAccess dataAccess = new DataAccess();
+				if (validate.IsPersonalNoInDb(UserName, dataAccess.LoadPersonalNumbers()))
+				{
+					OrderStatusView oldQuestView = new OrderStatusView(UserName);
+
+					//loginView.Close();
+					oldQuestView.Show();
+					var timer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(2) };
+					timer.Start();
+					timer.Tick += (sender, args) =>
+					{
+						timer.Stop();
+						NewGuestView guestView = new NewGuestView(UserName);
+						oldQuestView.Close();
+						guestView.Show();
+					};
+
+					return;
+				}
+				NewGuestView newGuestView = new NewGuestView(UserName);
+				loginView.Close();
+				newGuestView.Show();
+				return;
+			}
+			else
+			{
+				WarningView warning = new WarningView(loginView);
+				warning.Show("User name or password are not correct!");
+				return;
+			}
+		}
+	}
+}
